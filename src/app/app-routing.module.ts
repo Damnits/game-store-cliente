9b64eda78aef20fe3a9792ae6f57c93d39f1e358
Pablo <pablofats@hotmail.com>
2021-09-27 13:41:30 -0300

    auth incompleto com tela de cadastro e login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ListarProdutosComponent} from "./produto/listar-produtos/listar-produtos.component";
 import {CadastroProdutoComponent} from "./produto/cadastro-produto/cadastro-produto.component";
-import {AppComponent} from "./app.component";
+import { ListarClientesComponent } from './cliente/listar-clientes/listar-clientes.component';
+import { CadastrarClienteComponent } from './cliente/cadastrar-cliente/cadastrar-cliente.component';
+import { HomeComponent } from './home/login/home.component';
+import { CadastrousuarioComponent } from './home/cadastrousuario/cadastrousuario.component';
+import { AuthGuardService } from './auth/auth-guard.service';
 
 const routes: Routes = [
-  {path: '', component: ListarProdutosComponent},
-  {path:'listar-produtos', component: ListarProdutosComponent},
-  {path:'cadastrar-produtos', component: CadastroProdutoComponent}
-    ]
+  { path: '', component: HomeComponent  },
+  { path:'listar-produtos', component: ListarProdutosComponent, canActivate: [AuthGuardService] },
+  { path:'cadastrar-produtos', component: CadastroProdutoComponent, canActivate: [AuthGuardService] },
+  { path: 'listar-clientes', component: ListarClientesComponent, canActivate: [AuthGuardService] },
+  { path: 'cadastrar-cliente', component: CadastrarClienteComponent, canActivate: [AuthGuardService] },
+  { path: 'cadastrar-usuario', component: CadastrousuarioComponent}
+  ]
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NavigatorComponent } from './navigator/navigator.component';
-import {ProdutoModule} from "./produto/produto.module";
-import {HttpClientModule} from "@angular/common/http";
+import { ProdutoModule } from "./produto/produto.module";
+import { ClienteModule } from "./cliente/cliente.module";
+import { HttpClientModule } from "@angular/common/http";
+import { HomeComponent } from './home/login/home.component';
+import { CadastrousuarioComponent } from './home/cadastrousuario/cadastrousuario.component';
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
+
 @NgModule({
   declarations: [
     AppComponent,
     NavigatorComponent,
+    HomeComponent,
+    CadastrousuarioComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ProdutoModule,
+    ClienteModule,
     HttpClientModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [
 
